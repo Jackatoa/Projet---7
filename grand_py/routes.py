15ff0d30d003af8cde20321a050668a326da94d4
Jackatoa <xavier.endres@gmail.com>
2019-05-21 15:14:46 +0200

    rajout des fonctions permettant l'envoi et la reception des donnees

--- grand_py/routes.py
+++ grand_py/routes.py
@@ -1,17 +1,20 @@
-from flask import render_template, url_for, redirect, request
+from flask import render_template, url_for, redirect, request, jsonify
 from grand_py import app
 from grand_py.forms import EntryForm
+from grand_py.bot import Bot
 
 
 
 @app.route("/")
-@app.route("/home", methods=['GET', 'POST'])
+@app.route("/home")
 def home():
     form = EntryForm()
     return render_template('grandpy.html', form=form)
 
 
-@app.route("/about")
-def about():
-    return render_template('grandpy.html', title='About')
+@app.route("/bot", methods=['POST'])
+def bot():
+    bot = Bot()
+    return jsonify({'answer':
+                    bot.testtalk(request.form['text'])})
 
