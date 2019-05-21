f2be33330866ffb3a7817ad8dd57ed029a1d94be
Jackatoa <xavier.endres@gmail.com>
2019-05-21 15:12:59 +0200

    Premier ajout de réponse

---
+++ grand_py/bot.py
@@ -0,0 +1,11 @@
+from grand_py.messages import Message
+
+class Bot:
+    def __init__(self):
+        self.text = None
+
+    def grandpyTalk(self):
+        pass
+
+    def testtalk(self, text):
+        return "jai bien recu ce message" + text
