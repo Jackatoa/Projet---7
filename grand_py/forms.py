79e8577bf55e0a7e00efc930c2b06d2f8aa3e85e
Jackatoa <xavier.endres@gmail.com>
2019-05-21 15:18:46 +0200

    amélioration du formulaire en textarea

--- grand_py/forms.py
+++ grand_py/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import TextAreaField, SubmitField
+from wtforms.validators import DataRequired
 
 class EntryForm(FlaskForm):
-    entry = StringField('Entry')
+    entry = TextAreaField('entry',
+                           validators=[DataRequired()])
     submit = SubmitField('Ask GrandPy')
+
