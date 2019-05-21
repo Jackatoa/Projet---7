fd7b50c45daa2741e6de34d0564fb1b772f791b2
Jackatoa <xavier.endres@gmail.com>
2019-05-21 15:16:32 +0200

    fonction permettant l'affichage du message utilisateur et de la réponse

--- grand_py/static/js/grandpy.js
+++ grand_py/static/js/grandpy.js
@@ -1,25 +1,39 @@
+$(function () {
+    var $msg_card_body, EntryForm;
+    $msg_card_body = $('#msg_card_body');
+    EntryForm = $('#EntryForm');
+    $("#grandpy_writing").show();
+    EntryForm.on('submit', function (e) {
+        e.preventDefault();
+        var text = $.trim($('#text').val());
+        if (text.trim()){$msg_card_body.append('<div class="d-flex justify-content-end mb-4' +
+            ' main_user_img" id="user_msg_template">' +
+            '<div class="msg_cotainer_send">' +
+            '<p class="small">' + text + '</p>' +
+            '</div>' +
+            '<div class="img_cont_msg">' +
+            '<img src="../static/images/GrandSon.png"' +
+            ' class="rounded-circle user_img_msg">' +
+            '</div>' +
+            '</div>');
+        $.trim($('#text').val(''));
 
-$(function() {
-    var imageurl = "{{ url_for('static', filename='images/GrandSon.png') }}";
-  var $msg_card_body, EntryForm;
-  $msg_card_body = $('#msg_card_body');
-  EntryForm = $('#EntryForm');
+        $.post('/bot', {text: text,}).done(function (response) {
+            $("#grandpy_writing").hide();
+            $msg_card_body.append('<div class="d-flex justify-content-start mb-4" id="grandpy_msg_template">' +
+                '<div class="img_cont_msg">' +
+            '<img src="../static/images/GrandPy2.png" class="rounded-circle user_img_msg"></div>' +
+            '<div class="msg_cotainer">' +
+            '<p class="small">' + response['answer'] + '</p>' +
+            '</div>' +
+            '</div>');
+        })
 
-  EntryForm.on('submit', function(e) {
-    e.preventDefault();
-    var text = $.trim($('#text').val());
-    $msg_card_body.append('<div class="d-flex justify-content-end mb-4 main_user_img" id="user_msg_template">' +
-    '<div class="msg_cotainer_send">' +
-        '<p class="small user_msg_1">' + text + '</p>' +
-    '</div>' +
-    '<div class="img_cont_msg">' +
-        '<img src="../static/images/GrandSon.png"' +
-      ' class="rounded-circle user_img_msg">' +
-    '</div>' +
-'</div>');
-    $.trim($('#text').val(''));
-  });
 
+
+        }
+
+    });
 
 
 });
