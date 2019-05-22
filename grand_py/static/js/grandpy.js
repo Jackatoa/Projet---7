69c7f688626b62e415c6aeaec024649f63c0ce87
Jackatoa <xavier.endres@gmail.com>
2019-05-22 20:44:22 +0200

    protection du code pour les balises

--- grand_py/static/js/grandpy.js
+++ grand_py/static/js/grandpy.js
@@ -6,43 +6,46 @@ $(function () {
     EntryForm.on('submit', function (e) {
         e.preventDefault();
         var text = $.trim($('#text').val());
-        if (text.trim()){$msg_card_body.append('<div class="d-flex justify-content-end mb-4' +
-            ' main_user_img" id="user_msg_template">' +
-            '<div class="msg_cotainer_send">' +
-            '<p class="small">' + text + '</p>' +
-            '</div>' +
-            '<div class="img_cont_msg">' +
-            '<img src="../static/images/GrandSon.png"' +
-            ' class="rounded-circle user_img_msg">' +
-            '</div>' +
-            '</div>');
-        $.trim($('#text').val(''));
-        var pageHeight = $('#msg_card_body').height();
-        $('#msg_card_body').scrollTop(pageHeight);
-
-        $.post('/bot', {text: text,}).done(function (big_answer) {
-            $("#grandpy_writing").show();
-            setTimeout(function() {
-            $msg_card_body.append('<div class="d-flex justify-content-start mb-4" id="grandpy_msg_template">' +
+        text = text.split('<').join('&lt;');
+        text = text.split('>').join('&gt;');
+        if (text.trim()) {
+            $msg_card_body.append('<div class="d-flex justify-content-end mb-4' +
+                ' main_user_img" id="user_msg_template">' +
+                '<div class="msg_cotainer_send">' +
+                '<p class="small">' + text + '</p>' +
+                '</div>' +
                 '<div class="img_cont_msg">' +
-            '<img src="../static/images/GrandPy2.png" class="rounded-circle user_img_msg"></div>' +
-            '<div class="msg_cotainer">' +
-            '<p class="small">' + big_answer['answer'] + '</p>' +
-            '</div>' +
-            '</div>');
-            $("#grandpy_writing").hide();
-            playSound()
+                '<img src="../static/images/GrandSon.png"' +
+                ' class="rounded-circle user_img_msg">' +
+                '</div>' +
+                '</div>');
+            $.trim($('#text').val(''));
             var pageHeight = $('#msg_card_body').height();
             $('#msg_card_body').scrollTop(pageHeight);
-            }, 3000);
 
-        })
+            $.post('/bot', {text: text,}).done(function (big_answer) {
+                $("#grandpy_writing").show();
+                setTimeout(function () {
+                    $msg_card_body.append('<div class="d-flex justify-content-start mb-4" id="grandpy_msg_template">' +
+                        '<div class="img_cont_msg">' +
+                        '<img src="../static/images/GrandPy2.png" class="rounded-circle user_img_msg"></div>' +
+                        '<div class="msg_cotainer">' +
+                        '<p class="small">' + big_answer['answer'] + '</p>' +
+                        '</div>' +
+                        '</div>');
+                    $("#grandpy_writing").hide();
+                    playSound()
+                    var pageHeight = $('#msg_card_body').height();
+                    $('#msg_card_body').scrollTop(pageHeight);
+                }, 3000);
+
+            })
         }
     });
 });
 
 
 function playSound() {
-          var sound = document.getElementById("audio");
-          sound.play();
-      }
+    var sound = document.getElementById("audio");
+    sound.play();
+}
