bfb6cc92bdf87e296d7a23d688c1a3ea9c4182a6
Jackatoa <xavier.endres@gmail.com>
2019-05-24 21:06:14 +0200

    rajout de la map

--- grand_py/static/js/grandpy.js
+++ grand_py/static/js/grandpy.js
@@ -44,20 +44,30 @@ $(function () {
 
                     }
                     if (big_answer['map_answer']) {
-                        $msg_card_body.append('<div class="d-flex justify-content-start mb-4" id="grandpy_msg_template">' +
-                            '<div class="img_cont_msg">' +
+
+                        $msg_card_body.append('<div class="d-flex justify-content-start mb-4"' +
+                            ' id="grandpy_msg_template">' +
+                            '<div class="img_cont_msg" >' +
                             '<img src="../static/images/GrandPy2.png" class="rounded-circle user_img_msg"></div>' +
-                            '<div class="msg_cotainer">' +
-                            '<p class="small">' + big_answer['wiki_answer'] + '</p>' +
+                            '<div class="msg_cotainer" id="mapidbox">' +
+                            '<p class="small">' + big_answer['map_answer'] + '</p>' +
+                            '<div id="mapid">' +
+                            '</div>' +
                             '</div>' +
                             '</div>');
+                        var mymap = L.map('mapid').setView([big_answer['answer_lat'], big_answer['answer_long']], 13);
+                        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+                            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                            maxZoom: 18,
+                            id: 'mapbox.streets',
+                            accessToken: 'pk.eyJ1IjoiamFja2F0b2EiLCJhIjoiY2p3MmVwNW5yMDVzbzN5cW55bjVxdWhidCJ9.jtwQWGRlS8_emw5VM23VLg'
+                        }).addTo(mymap);
 
                     }
                     $("#grandpy_writing").hide();
                     playSound()
                     $("#msg_card_body").stop().animate({scrollTop: $("#msg_card_body")[0].scrollHeight}, 1000);
                 }, 3000);
-
             })
         }
     });
@@ -68,24 +78,4 @@ function playSound() {
     var sound = document.getElementById("audio");
     sound.play();
 };
-var lat = 48.852969;
-var lon = 2.349903;
-var macarte = null;
 
-// Fonction d'initialisation de la carte
-function initMap() {
-    // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
-    macarte = L.map('map').setView([lat, lon], 11);
-    // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
-    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-        // Il est toujours bien de laisser le lien vers la source des données
-        attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
-        minZoom: 1,
-        maxZoom: 20
-    }).addTo(macarte);
-}
-
-window.onload = function () {
-    // Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
-    initMap();
-};
