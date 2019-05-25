30098e0e05c15e6728aa8d317f6b972bd8ea6f01
Jackatoa <xavier.endres@gmail.com>
2019-05-25 11:06:16 +0200

    amélioration des réponses

--- grand_py/answer.py
+++ grand_py/answer.py
@@ -1,11 +1,12 @@
-
-
 class Answer():
     answer_stupid = ["Tu es bon en algèbre mon petit !", "Tu es bien silencieux mon petit !",
-                     "Ne soit pas interloqué mon petit !", "Réveille toi mon petit !", "Arrête de grommeler mon petit !"]
+                     "Ne soit pas interloqué mon petit !", "Réveille toi mon petit !",
+                     "Arrête de grommeler mon petit !", "Mais de rien mon petit !"]
     answer_error = ["reponse erreur"]
     answer_wiki_find = ["page wiki trouvee"]
     answer_location_find = ["endroit trouvé"]
+    answer_too_old = ["Tu sais je suis un peu dépassé avec les termes que vous employez vous " \
+                      "les jeunes. Ne voudrais pas tu partager un werther's original plutôt ?"]
 
     def __init__(self):
         pass
@@ -21,3 +22,6 @@ class Answer():
 
     def get_location_answer(self):
         return Answer.answer_location_find[0]
+
+    def get_old_answer(self):
+        return Answer.answer_too_old[0]
