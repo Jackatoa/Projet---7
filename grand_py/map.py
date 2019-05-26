af77fab40099f63cb3092f9505b7386cd3b564f8
Jackatoa <xavier.endres@gmail.com>
2019-05-26 08:50:59 +0200

    amélioration des recherches googles maps

--- grand_py/map.py
+++ grand_py/map.py
@@ -3,19 +3,21 @@ import requests
 
 class Map:
 
-    def __init__(self, search):
+    def __init__(self, address, place):
         self.api_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
-        self.search = search
+        self.search = address
+        self.place = place
         self.parameters = {
             'address': self.search,
-            'key': 'AIzaSyBlM1lBFayUTLiN8ZiwczP6DWfWGNNYlT8'
+            'key'    : 'AIzaSyBlM1lBFayUTLiN8ZiwczP6DWfWGNNYlT8'
         }
         self.response = requests.get(url=self.api_url, params=self.parameters).json()
+        self.data = None
         self.location = None
         self.latitude = None
         self.longitude = None
 
-    def exist(self):
+    def adress_exist(self):
         if not self.response['results']:
             return False
         else:
@@ -24,11 +26,17 @@ class Map:
             self.longitude = self.location['lng']
             return True
 
-
-
-
-
-
-
-
+    def location_exist(self):
+        url = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
+        self.place += " france"
+        parameters = {
+            'input'    : self.place,
+            'key'      : 'AIzaSyBlM1lBFayUTLiN8ZiwczP6DWfWGNNYlT8',
+            'inputtype': 'textquery',
+            'language' : 'fr',
+            'fields'   : 'geometry/location'
+        }
+        self.response = requests.get(url=url, params=parameters).json()
+        if self.response['results']:
+            return True
 
