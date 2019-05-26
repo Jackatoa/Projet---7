106f41bb6df4b6093825ce2c831af5511a80ea39
Jackatoa <xavier.endres@gmail.com>
2019-05-26 08:58:04 +0200

    ajout de la doc

--- grand_py/routes.py
+++ grand_py/routes.py
@@ -8,12 +8,14 @@ from grand_py.bot import Bot
 @app.route("/")
 @app.route("/home")
 def home():
+    #Main page with chat
     form = EntryForm()
     return render_template('grandpy.html', form=form)
 
 
 @app.route("/bot", methods=['POST'])
 def bot():
+    #Used for bot questionning
     bot = Bot(request.form['text'])
     return bot.grandpyTalk()
 
