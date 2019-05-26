f3e3f155434a729df36ec5967c802c227e9e5ad3
Jackatoa <xavier.endres@gmail.com>
2019-05-26 08:50:25 +0200

    amélioration de l'algorithme du parser

--- grand_py/bot.py
+++ grand_py/bot.py
@@ -4,6 +4,7 @@ from grand_py.wiki import Wiki
 from grand_py.answer import Answer
 from grand_py.map import Map
 from grand_py.parser import Parser
+from unidecode import unidecode
 
 class Bot:
     def __init__(self, question):
@@ -13,18 +14,20 @@ class Bot:
         self.coord_lat = None
         self.coord_long = None
         self.question = question
+        self.wikiquestion = None
+        self.mapquestion = None
 
     def json_answer(self):
         if self.map_answer and self.wiki_answer:
-            return jsonify({'answer': self.answer,
+            return jsonify({'answer'     : self.answer,
                             'wiki_answer': self.wiki_answer,
-                            'map_answer': self.map_answer,
-                            'answer_lat': self.coord_lat,
+                            'map_answer' : self.map_answer,
+                            'answer_lat' : self.coord_lat,
                             'answer_long': self.coord_long})
         elif self.map_answer:
-            return jsonify({'answer': self.answer,
-                            'map_answer': self.map_answer,
-                            'answer_lat': self.coord_lat,
+            return jsonify({'answer'     : self.answer,
+                            'map_answer' : self.map_answer,
+                            'answer_lat' : self.coord_lat,
                             'answer_long': self.coord_long})
         elif self.wiki_answer:
             return jsonify({'answer': self.answer, 'wiki_answer': self.wiki_answer})
@@ -35,14 +38,22 @@ class Bot:
         if self.big_check():
             return self.json_answer()
         else:
-            self.clean_question()
+            self.question = unidecode(self.question)
+            self.question = self.clean_question(Parser.stop_words)
+            self.wikiquestion = Wiki(self.clean_question(Parser.locationwords))
+            print(self.clean_question(Parser.locationwords))
+            self.mapquestion = Map(self.question, self.clean_question(Parser.locationwords))
             self.grandpy_try()
             return self.json_answer()
 
-    def clean_question(self):
+    def clean_question(self, lst):
+        self.question = self.question.replace("'", ' ')
         list_question = self.question.split(" ")
-        new_list_question = [x for x in list_question if x not in Parser.stop_words]
-        self.question = ' '.join(new_list_question)
+        new_list_question = []
+        for x in list_question:
+            new_list_question.append(re.sub('[^A-Za-z0-9]+', '', x.lower()))
+        new_list_question = [x for x in new_list_question if x.lower() not in lst]
+        return ' '.join(new_list_question)
 
     def big_check(self):
         self.answer = Answer()
@@ -68,39 +79,50 @@ class Bot:
             return False
 
     def grandpy_try(self):
-        wikianswer = Wiki(self.question)
-        streetanswer = Map(self.question)
-        if self.check_adress() and streetanswer.exist():
-            self.grandpy_find_adress(streetanswer)
-        elif any(word in self.question for word in Parser.locationwords):
+        print(self.mapquestion.response['results'])
+        print(self.question.split())
+        if self.check_adress() and self.mapquestion.adress_exist():
+            self.grandpy_find_adress()
+        elif any(word in self.question.split() for word in Parser.adresslst) and \
+                self.mapquestion.location_exist():
             self.grandpy_find_location()
-        elif wikianswer.exist():
-            self.grandpy_find_wiki(wikianswer)
+        elif self.wikiquestion.exist():
+            self.grandpy_find_wiki()
         else:
-            self.answer = self.answer.get_old_answer(0)
+            self.answer = self.answer.get_old_answer()
 
     def check_adress(self):
         if any(word in self.question for word in Parser.adresslst):
             return True
 
-    def grandpy_find_adress(self, streetanswer):
-            self.answer = "Je connais cet endroit, il est juste ici :"
-            self.map_answer = "INFOS OPEN STREET MAP"
-            self.coord_lat = streetanswer.latitude
-            self.coord_long = streetanswer.longitude
-            return self.json_answer()
+    def grandpy_find_adress(self):
+        self.answer = a.random_answer(a.answer_location_find)
+        self.map_answer = a.random_answer(a.answer_location_here)
+        self.coord_lat = self.mapquestion.latitude
+        self.coord_long = self.mapquestion.longitude
+        return self.json_answer()
 
-    def grandpy_find_wiki(self, wikianswer):
-        self.answer = "Je peux te dire beaucoup sur ce sujet "
-        if wikianswer.is_location():
-            self.map_answer = "Voilà son emplacement :"
-            self.coord_lat = wikianswer.lat
-            self.coord_long = wikianswer.long
-            self.wiki_answer = wikianswer.getSummary()
+    def grandpy_find_wiki(self):
+        self.answer = a.random_answer(a.answer_wiki_find)
+        if self.wikiquestion.is_location():
+            self.map_answer = a.random_answer(a.answer_location_here)
+            self.coord_lat = self.wikiquestion.lat
+            self.coord_long = self.wikiquestion.long
+            self.wiki_answer = self.wikiquestion.getSummary()
             return self.json_answer()
         else:
-            self.wiki_answer = wikianswer.getSummary()
+            self.wiki_answer = self.wikiquestion.getSummary()
             return self.json_answer()
 
     def grandpy_find_location(self):
-        pass
+        self.coord_lat = self.mapquestion.response['results'][0]['geometry']['location'][
+            'lat']
+        self.coord_long = self.mapquestion.response['results'][0]['geometry']['location'][
+            'lng']
+        self.answer = a.random_answer(a.answer_location_find)
+        self.map_answer = a.random_answer(a.answer_location_here)
+        if self.wikiquestion.exist():
+            self.wiki_answer = self.wikiquestion.getSummary()
+        return self.json_answer()
+
+a = Answer()
